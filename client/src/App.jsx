af451923f3ec2b1adfd904b944714c26b15d6eef
Vriddhi Vashi <vvriddhi@gmail.com>
2025-04-19 12:25:11 +0530

    basic

---
+++ client/src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+
+const App = () => {
+  return (
+    <div className="bg-red-900 text-center text-2xl p-4">
+      Tailwind is working!
+    </div>
+  )
+}
+
+export default App
